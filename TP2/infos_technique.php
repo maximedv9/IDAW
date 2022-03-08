2a026dd23a305a8e263197772054936a16a95642
Maxime de Veyrac <maximedeveyrac@hotmail.fr>
2022-03-08 15:28:55 +0100

    infos technique rajouté au menu

---
+++ TP2/infos_technique.php
@@ -0,0 +1,26 @@
+<?php
+require_once('template_header.php');
+?>
+
+<body>
+    <?php
+    require_once('template_menu.php')
+    ?>
+    <section class="showcase">
+        <div class="container grid">
+            <div class="card">
+                <h1>Infos Technique</h1>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam
+                    ducimus est recusandae nemo amet rerum nesciunt sit, illum
+                    consectetur quidem laborum corrupti mollitia beatae, sapiente vero!
+                    Voluptates dicta rerum aut!
+                </p>
+                <a href="features.php">Read More</a>
+            </div>
+        </div>
+    </section>
+    <?php
+    require_once('template_footer.php');
+    ?>
+</body>
--- TP2/template_menu.php
+++ TP2/template_menu.php
@@ -9,6 +9,7 @@
                 <li><a href="index.php">Home</a></li>
                 <li><a href="cv.php">CV</a></li>
                 <li><a href="projects.php">Projects</a></li>
+                <li><a href="infos_technique.php">Infos Technique</a></li>
             </ul>
         </nav>
     </div>
