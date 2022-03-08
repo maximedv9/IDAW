30a6527fc1957fc93619bd088713913162d6174b
Maxime de Veyrac <maximedeveyrac@hotmail.fr>
2022-03-08 14:56:51 +0100

    Exo 1 date

---
+++ TP2/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html class="h-100">
+
+<head>
+    <title>Date</title>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+
+<body>
+    <h1>La date est :</h1>
+    <?php
+    echo date("d/m/y")
+    ?>
+</body>
