a8633da2568848a0b6cf10970181ab3058775d7a
Maxime de Veyrac <maximedeveyrac@hotmail.fr>
2022-03-08 15:04:21 +0100

    rennomer en php

---
+++ TP2/cv.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html class="h-100">
+  <head>
+    <title>CV | Portfolio</title>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+  <body>
+    <div class="navbar">
+      <div class="container flex">
+        <h1 class="logo">Portfolio</h1>
+        <h2 class="logo">Maxime de Veyrac</h2>
+        <nav>
+          <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a id="currentpage" href="cv.html">CV</a></li>
+            <li><a href="projects.html">Projects</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+    <section class="showcase">
+      <div class="container">
+        <div class="cvcard">
+          <img src="photo_cv.jpg" style="width: 100px; height: 120px" />
+          <h2>Ceci est un CV</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. A ipsam
+            labore reiciendis ex unde architecto suscipit magnam, esse vero,
+            numquam modi porro dolores magni! Rerum necessitatibus at vitae
+            corrupti nihil. Lorem ipsum dolor sit, amet consectetur adipisicing
+            elit. Eos a, libero saepe incidunt ab, recusandae voluptate vel est,
+            minus consequatur dolorum dolore corrupti. Provident libero
+            excepturi incidunt odit repudiandae ut. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Cupiditate earum nisi totam nulla
+            possimus ad corrupti, aliquid recusandae sunt, tempore fuga. Nemo
+            earum dolorem, voluptas sunt est illum ipsa aliquam.
+          </p>
+        </div>
+      </div>
+    </section>
+    <footer class="bottom">
+      <div class="container flex">
+        <p>IMT Nord Europe</p>
+        <p>IDAW 2020</p>
+        <p>Maxime de Veyrac</p>
+      </div>
+    </footer>
+  </body>
+</html>
--- TP2/index.php
+++ TP2/index.php
@@ -2,14 +2,46 @@
 <html class="h-100">
 
 <head>
-    <title>Date</title>
-    <meta charset="utf-8" />
-    <link rel="stylesheet" href="css/style.css" />
+  <title>Home | Portfolio</title>
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="css/style.css" />
 </head>
 
 <body>
-    <h1>La date est :</h1>
-    <?php
-    echo date("d/m/y")
-    ?>
+  <div class="navbar">
+    <div class="container flex">
+      <h1 class="logo">Portfolio</h1>
+      <h2 class="logo">Maxime de Veyrac</h2>
+      <nav>
+        <ul>
+          <li><a id="currentpage" href="index.html">Home</a></li>
+          <li><a href="cv.html">CV</a></li>
+          <li><a href="projects.html">Projects</a></li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+  <section class="showcase">
+    <div class="container grid">
+      <div class="card">
+        <h1>Home</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam
+          ducimus est recusandae nemo amet rerum nesciunt sit, illum
+          consectetur quidem laborum corrupti mollitia beatae, sapiente vero!
+          Voluptates dicta rerum aut!
+        </p>
+        <a href="features.html">Read More</a>
+      </div>
+    </div>
+  </section>
+  <footer class="bottom">
+    <div class="container flex">
+      <p>IMT Nord Europe</p>
+      <p>IDAW 2020</p>
+      <p>Maxime de Veyrac</p>
+    </div>
+  </footer>
 </body>
+
+</html>
