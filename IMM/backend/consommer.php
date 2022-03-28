e889e40ced1253942ac95a7bde09716b5ccb224e
Maxime de Veyrac <maximedeveyrac@hotmail.fr>
2022-03-28 14:18:47 +0200

    indent et url

--- IMM/backend/consommer.php
+++ IMM/backend/consommer.php
@@ -1,33 +1,30 @@
 
 
 <?php
-    $servername = 'localhost';
-    $username = 'root';
-    $password = 'root';
-    $dbname = 'iMM';
+$servername = 'localhost';
+$username = 'root';
+$password = 'root';
+$dbname = 'iMM';
 
-    //essaye de se connecter
-    try{
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname",$username,$password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
-        //Sélectionne toutes les valeurs dans la table consommer et les affiche
-        $sql = "SELECT aliment.LIB_ALIMENT,consommer.QUANTITE,consommer.DATE FROM `consommer` JOIN `aliment` ON consommer.ID_ALIMENT=aliment.ID_ALIMENT";
-        // $res['req']=$sql;
+//essaye de se connecter
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $result = $conn->query($sql);
-        $getAll = $result->fetchAll();
-        
-        if(!$getAll)
-            $getAll['status']='Fail';
-    
-        echo json_encode($getAll);
-        
-    }
+    //Sélectionne toutes les valeurs dans la table consommer et les affiche
+    $sql = "SELECT aliment.LIB_ALIMENT,consommer.QUANTITE,consommer.DATE FROM `consommer` JOIN `aliment` ON consommer.ID_ALIMENT=aliment.ID_ALIMENT";
+    // $res['req']=$sql;
 
-    catch(PDOException $e){
-        echo "Erreur : " . $e->getMessage();
-    }
-    
+    $result = $conn->query($sql);
+    $getAll = $result->fetchAll();
+
+    if (!$getAll)
+        $getAll['status'] = 'Fail';
+
+    echo json_encode($getAll);
+} catch (PDOException $e) {
+    echo "Erreur : " . $e->getMessage();
+}
+
 ?>
 
