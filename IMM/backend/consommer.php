e9086166d422700294b3dbde814e7a0c43960680
Hugo <hugolim2000@gmail.com>
2022-03-29 15:27:48 +0200

    adresse mail en dur

--- IMM/backend/consommer.php
+++ IMM/backend/consommer.php
@@ -1,28 +1,32 @@
 <?php
-$servername = 'localhost';
-$username = 'root';
-$password = 'root';
-$dbname = 'iMM';
+    $servername = 'localhost';
+    $username = 'root';
+    $password = 'root';
+    $dbname = 'iMM';
 
-//essaye de se connecter
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //essaye de se connecter
+    try{
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname",$username,$password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    //Sélectionne toutes les valeurs dans la table consommer et les affiche
-    $sql = "SELECT aliment.LIB_ALIMENT,consommer.QUANTITE,consommer.DATE FROM `consommer` JOIN `aliment` ON consommer.ID_ALIMENT=aliment.ID_ALIMENT";
-    // $res['req']=$sql;
+        
+        //Sélectionne toutes les valeurs dans la table consommer et les affiche
+        $sql = "SELECT aliment.LIB_ALIMENT,consommer.QUANTITE,consommer.DATE FROM `consommer` JOIN `aliment` ON consommer.ID_ALIMENT=aliment.ID_ALIMENT WHERE EMAIL='hugo.lim@etu.imt-lille-douai.fr' ORDER BY consommer.DATE DESC";
+        // $res['req']=$sql;
 
-    $result = $conn->query($sql);
-    $getAll = $result->fetchAll();
+        $result = $conn->query($sql);
+        $getAll = $result->fetchAll();
+        
+        if(!$getAll)
+            $getAll['status']='Fail';
+    
+        echo json_encode($getAll);
+        
+    }
 
-    if (!$getAll)
-        $getAll['status'] = 'Fail';
-
-    echo json_encode($getAll);
-} catch (PDOException $e) {
-    echo "Erreur : " . $e->getMessage();
-}
-
+    catch(PDOException $e){
+        echo "Erreur : " . $e->getMessage();
+    }
+    
 ?>
 
--- IMM/frontend/template_menu.php
+++ IMM/frontend/template_menu.php
@@ -6,15 +6,10 @@ function renderMenuToHTML($currentPageId)
         'home' => 'Accueil',
         'profile_page' => 'Profil',
         'statistiques_page' => 'Statistiques',
-        'liste_aliments_page' => 'Aliments',
-
-<<<<<<< HEAD
-
+        'liste_aliments_page' => 'Aliments'
     );
 
 
-=======
->>>>>>> a5acae811a818b927f5fbf9c641e4e2e25fd1d90
     echo '<div class="navbar"><div class="container flex"><h1 class="logo">Portfolio</h1>';
 
     if (isset($_SESSION['login'])) {
