80fe8faadecf7caacc0f2cf0c5b98d004e106b31
Maxime de Veyrac <maximedeveyrac@hotmail.fr>
2022-03-29 16:05:30 +0200

    git conflits

--- IMM/frontend/profile_page.php
+++ IMM/frontend/profile_page.php
@@ -11,37 +11,36 @@
                 <table class="table">
                     <thead>
                         <tr>
-                        <th scope="col">Nom</th>
-                        <th scope="col">Prénom</th>
-                        <th scope="col">Email</th>
+                            <th scope="col">Nom</th>
+                            <th scope="col">Prénom</th>
+                            <th scope="col">Email</th>
                         </tr>
                     </thead>
-        <tbody id="profileTableBody">
-        </tbody>
-    </table>
+                    <tbody id="profileTableBody">
+                    </tbody>
+                </table>
             </div>
         </div>
     </section>
     <script>
-        $.get('http://localhost:8888/IDAW-1/IMM/backend/user_profile.php', function(data){
+        $.get('http://localhost:8888/IDAW-1/IMM/backend/user_profile.php', function(data) {
                 console.debug(data);
                 data = JSON.parse(data);
-                data.forEach(row =>{
+                data.forEach(row => {
                     $("#profileTableBody").append(`<tr><td>${row[0]}</td><td>${row[1]}</td><td>${row[2]}</td></tr>`)
                 });
             })
-        
-        .done(function(data){
-            console.log("La requête a été un succès");
-        })
-        
-        .fail(function(){
-            console.log("La requête s'est terminée en échec")
-        })
 
-        .always(function(){
-            console.log("Requête effectuée");
-        })
+            .done(function(data) {
+                console.log("La requête a été un succès");
+            })
 
-        </script>
+            .fail(function() {
+                console.log("La requête s'est terminée en échec")
+            })
+
+            .always(function() {
+                console.log("Requête effectuée");
+            })
+    </script>
 </body>
