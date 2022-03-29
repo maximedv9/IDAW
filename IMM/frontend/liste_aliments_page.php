347d510c3bf1ededc842c4afa06ddc17f80658fe
Maxime de Veyrac <maximedeveyrac@hotmail.fr>
2022-03-29 15:59:16 +0200

    pull

--- IMM/frontend/liste_aliments_page.php
+++ IMM/frontend/liste_aliments_page.php
@@ -11,35 +11,34 @@
                 <table class="table">
                     <thead>
                         <tr>
-                        <th scope="col">Aliment</th>
+                            <th scope="col">Aliment</th>
                         </tr>
                     </thead>
-        <tbody id="alimentsTableBody">
-        </tbody>
-    </table>
+                    <tbody id="alimentsTableBody">
+                    </tbody>
+                </table>
             </div>
         </div>
     </section>
     <script>
-        $.get('http://localhost:8888/IDAW-1/IMM/backend/aliments.php', function(data){
+        $.get('http://localhost:8888/IDAW-1/IMM/backend/aliments.php', function(data) {
                 console.debug(data);
                 data = JSON.parse(data);
-                data.forEach(row =>{
+                data.forEach(row => {
                     $("#alimentsTableBody").append(`<tr><td>${row[0]}</td></tr>`)
                 });
             })
-        
-        .done(function(data){
-            console.log("La requête a été un succès");
-        })
-        
-        .fail(function(){
-            console.log("La requête s'est terminée en échec")
-        })
 
-        .always(function(){
-            console.log("Requête effectuée");
-        })
+            .done(function(data) {
+                console.log("La requête a été un succès");
+            })
 
-        </script>
+            .fail(function() {
+                console.log("La requête s'est terminée en échec")
+            })
+
+            .always(function() {
+                console.log("Requête effectuée");
+            })
+    </script>
 </body>
