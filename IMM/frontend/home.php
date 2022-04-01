02226316aacd7ae652f73c2b28f09482587e98c1
Maxime de Veyrac <maximedeveyrac@hotmail.fr>
2022-04-01 18:32:45 +0200

    chart nutriment

--- IMM/frontend/home.php
+++ IMM/frontend/home.php
@@ -24,43 +24,34 @@
 
         chart.legend = new am4charts.Legend();
 
-        chart.data = [{
-                country: "Lithuania",
-                litres: 501.9
-            },
-            {
-                country: "Czech Republic",
-                litres: 301.9
-            },
-            {
-                country: "Ireland",
-                litres: 201.1
-            },
-            {
-                country: "Germany",
-                litres: 165.8
-            },
-            {
-                country: "Australia",
-                litres: 139.9
-            },
-            {
-                country: "Austria",
-                litres: 128.3
-            },
-            {
-                country: "UK",
-                litres: 99
-            },
-            {
-                country: "Belgium",
-                litres: 60
-            },
-            {
-                country: "The Netherlands",
-                litres: 50
-            }
-        ];
+        val1 = {
+            Nutriments: document.getElementById("name0").innerHTML,
+            Quantité: document.getElementById("qte0").innerHTML,
+
+        }
+        val2 = {
+            Nutriments: document.getElementById("name1").innerHTML,
+            Quantité: document.getElementById("qte1").innerHTML,
+
+        }
+        val3 = {
+            Nutriments: document.getElementById("name2").innerHTML,
+            Quantité: document.getElementById("qte2").innerHTML,
+
+        }
+        val4 = {
+            Nutriments: document.getElementById("name3").innerHTML,
+            Quantité: document.getElementById("qte3").innerHTML,
+
+        }
+
+        chart.data = [];
+        chart.data.push(val1);
+        chart.data.push(val2);
+        chart.data.push(val3);
+        chart.data.push(val4);
+
+        console.log(chart.data);
 
         chart.innerRadius = 100;
 
@@ -76,6 +67,19 @@
         <div class="container grid">
             <div class="cardchart">
                 <div id="chartdiv"></div>
+            </div>
+            <div class="card">
+                <h2>Nutriments consommés</h2>
+                <table class="table" id="nutrimentsTable">
+                    <thead>
+                        <tr>
+                            <th scope="col">Nutriment</th>
+                            <th scope="col">Quantité</th>
+                        </tr>
+                    </thead>
+                    <tbody id="nutrimentsTableBody">
+                    </tbody>
+                </table>
             </div>
             <div class="card">
                 <h2>Historique des aliments consommés</h2>
@@ -97,6 +101,28 @@
                 data = JSON.parse(data);
                 data.forEach(row => {
                     $("#mealsTableBody").append(`<tr><td>${row[0]}</td><td>${row[1]}</td><td>${row[2]}</td></tr>`)
+                });
+            })
+
+            .done(function(data) {
+                console.log("La requête a été un succès");
+            })
+
+            .fail(function() {
+                console.log("La requête s'est terminée en échec")
+            })
+
+            .always(function() {
+                console.log("Requête effectuée");
+            })
+
+        $.get('http://localhost/IDAW/IMM/backend/nutriments.php', function(data) {
+                console.debug(data);
+                data = JSON.parse(data);
+                var i = 0;
+                data.forEach(row => {
+                    $("#nutrimentsTableBody").append(`<tr><td id="name${i}">${row[0]}</td><td id="qte${i}">${row[1]}</td></tr>`);
+                    i++;
                 });
             })
 
--- IMM/frontend/liste_aliments_page.php
+++ IMM/frontend/liste_aliments_page.php
@@ -4,34 +4,34 @@
 <body>
     <section class="showcase">
         <div class="container grid">
-        <form id="addMealForm" action="" onsubmit="onFormSubmit();">
+            <form id="addMealForm" action="" onsubmit="onFormSubmit();">
 
-        <p id="food-name"></p>
+                <p id="food-name"></p>
 
-        <div class="form-group row">
-            <label for="inputQte" class="col-sm-2 col-form-label">Quantité</label>
-            <div class="col-sm-3">
-                <input type="text" class="form-control" id="inputQte" >
-            </div>
-        </div>
+                <div class="form-group row">
+                    <label for="inputQte" class="col-sm-2 col-form-label">Quantité</label>
+                    <div class="col-sm-3">
+                        <input type="text" class="form-control" id="inputQte">
+                    </div>
+                </div>
 
-        <div class="form-group row">
-            <label for="inputDate" class="col-sm-2 col-form-label">Date</label>
-            <div class="col-sm-3">
-                <input type="datetime-local" class="form-control" id="inputDate" >
-            </div>
-        </div>
+                <div class="form-group row">
+                    <label for="inputDate" class="col-sm-2 col-form-label">Date</label>
+                    <div class="col-sm-3">
+                        <input type="datetime-local" class="form-control" id="inputDate">
+                    </div>
+                </div>
 
-        <br>
+                <br>
 
-        <div class="form-group row">
-            <span class="col-sm-2"></span>
-            <div class="col-sm-2">
-                <button type="submit" class="btn btn-primary form-control">Submit</button>
-            </div>
-        </div>
+                <div class="form-group row">
+                    <span class="col-sm-2"></span>
+                    <div class="col-sm-2">
+                        <button type="submit" class="btn btn-primary form-control">Submit</button>
+                    </div>
+                </div>
 
-    </form>
+            </form>
             <div class="card">
                 <h2>Table des aliments</h2>
                 <table id="alimentsTable" class="table">
@@ -49,20 +49,10 @@
 
     </section>
     <script>
-<<<<<<< HEAD
-        $(document).ready(function() {
-            $.get('http://localhost/IDAW/IMM/backend/aliments.php', function(data) {
-                    console.debug(data);
-                    data = JSON.parse(data);
-                    data.forEach(row => {
-                        $("#alimentsTableBody").append(`<tr><td>${row[0]}</td></tr>`)
-                    });
-                })
-=======
         // cacher le formulaire d'ajout d'aliment lorsque l'on arrive sur la page
         document.getElementById("addMealForm").style.display = 'none';
 
-        $.get('http://localhost:8888/IDAW-1/IMM/backend/aliments.php', function(data) {
+        $.get('http://localhost/IDAW/IMM/backend/aliments.php', function(data) {
                 console.debug(data);
                 data = JSON.parse(data);
                 let id_row = 0;
@@ -71,7 +61,42 @@
                     id_row++;
                 });
             })
->>>>>>> 555f294809dc45b3d22eeccb563a1c7c93189ed1
+
+            .done(function(data) {
+                console.log("La requête a été un succès");
+            })
+
+            .fail(function() {
+                console.log("La requête s'est terminée en échec")
+            })
+
+            .always(function() {
+                console.log("Requête effectuée");
+            })
+        //Utilisation de datatable pour la mise en forme et la pagination des aliments
+        $(document).ready(function() {
+            $('#alimentsTable').DataTable();
+        });
+
+
+        function onFormSubmit() {
+            // prevent the form to be sent to the server
+            event.preventDefault();
+            let qte = $("#inputQte").val();
+            let date = $("#inputDate").val();
+
+            //Il faut que la date et la quantité soit non vide pour que l'on puisse ajouter un aliment
+            let data = {
+                email: 'hugo.lim@etu.imt-lille-douai.fr',
+                qte: qte,
+                date: date,
+                foodname: document.getElementById("food-name").innerHTML
+            }
+
+            console.log(data);
+
+            //post des données dans la database
+            $.post("http://localhost/IDAW/IMM/backend/add_consommer.php", data, "json")
 
                 .done(function(data) {
                     console.log("La requête a été un succès");
@@ -81,54 +106,19 @@
                     console.log("La requête s'est terminée en échec")
                 })
 
-            .always(function() {
-                console.log("Requête effectuée");
-            })
-        //Utilisation de datatable pour la mise en forme et la pagination des aliments
-        $(document).ready( function () {
-            $('#alimentsTable').DataTable();
-        } );
-
-
-        function onFormSubmit() {
-        // prevent the form to be sent to the server
-            event.preventDefault();
-            let qte = $("#inputQte").val();
-            let date = $("#inputDate").val();
-
-        //Il faut que la date et la quantité soit non vide pour que l'on puisse ajouter un aliment
-            let data = {
-                email: 'hugo.lim@etu.imt-lille-douai.fr',
-                qte: qte,
-                date: date,
-                foodname: document.getElementById("food-name").innerHTML
-            }
-
-        console.log(data);
-
-        //post des données dans la database
-        $.post("http://localhost:8888/IDAW-1/IMM/backend/add_consommer.php",data,"json")
-
-        .done(function(data){
-            console.log("La requête a été un succès");
-        })
-        
-        .fail(function(){
-            console.log("La requête s'est terminée en échec")
-        })
-
-    }
+        }
 
 
         //Permet d'afficher le formulaire d'ajout d'aliment une fois que l'on a appuyé sur le bouton ajouter
-        function addMeal(index){
+        function addMeal(index) {
             var food_name = document.getElementById(index).innerHTML;
             console.log(index);
             var x = document.getElementById("addMealForm");
             document.getElementById("food-name").innerHTML = food_name;
             x.style.display = "block";
-            $('html, body').animate({ scrollTop: 0 }, 'fast');
+            $('html, body').animate({
+                scrollTop: 0
+            }, 'fast');
         }
-
     </script>
 </body>
